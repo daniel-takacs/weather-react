9e431a3693081a514baf125997d1db3132bd668c
daniel-takacs <takacsd89@gmail.com>
2022-02-18 12:43:29 +0200

    weather ID added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Home.jsx'
 import WeatherCard from './WeatherCard'
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -123,88 +123,87 @@ import React, { useState, useEffect } from "react";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Home() {
-  const [getCity, setGetCity] = useState("london");
-  const [data, setData] = useState([]);
-  const [weather, setWeather] = useState([]);
-  const [input, setInput] = useState("");
+    const [getCity, setGetCity] = useState("london");
+    const [data, setData] = useState([]);
+    const [weather, setWeather] = useState([]);
+    const [input, setInput] = useState("");
 
-  useEffect(() => {
-    const fetchingData = () => {
-      fetch(
-        `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${getCity}`
-      )
-        .then((res) => {
-          if (res.ok) {
-            console.log(res.status);
-            return res.json();
-          } else {
-            if (res.status === 404) {
-              return alert("wrong city");
-            }
-            alert("something went wrong");
-            throw new Error("you have an error");
-          }
-        })
-        .then((result) => {
-          console.log(result);
-          let { Key } = result[0];
-          let { LocalizedName } = result[0];
-          setData({ Key, LocalizedName });
-        })
-        .catch((error) => console.log(error));
-    };
-    fetchingData();
-  }, [getCity]);
+    useEffect(() => {
+        const fetchingData = () => {
+            fetch(
+                `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${getCity}`
+            )
+                .then((res) => {
+                    if (res.ok) {
+                        console.log(res.status);
+                        return res.json();
+                    } else {
+                        if (res.status === 404) {
+                            return alert("wrong city");
+                        }
+                        alert("something went wrong");
+                        throw new Error("you have an error");
+                    }
+                })
+                .then((result) => {
+                    console.log(result);
+                    let { Key } = result[0];
+                    let { LocalizedName } = result[0];
+                    let { ID } = result[0].Country
+                    setData({ Key, LocalizedName, ID });
+                })
+                .catch((error) => console.log(error));
+        };
+        fetchingData();
+    }, [getCity]);
 
-  let getKey = data.Key;
- 
-
-  const handleSubmit = (e)=> {
-      e.preventDefault();
-      setGetCity(input)
-      const fetchingWeather = () => {
-        fetch(
-          `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${getKey}?apikey=${API_KEY}&metric=true`
-        )
-          .then((res) => {
-            if (res.ok) {
-              console.log(res.status);
-              return res.json();
-            } else {
-              if (res.status === 404) {
-                return alert("error");
-              }
-              alert("something went wrong");
-              throw new Error("you have an error");
-            }
-          })
-          .then((result) => {
-            console.log('weather', result);
-            let { Value } = result.DailyForecasts[0].Temperature.Maximum;
-            let { Text } = result.Headline;
-            setWeather({ Value, Text });
-          })
-          .catch((error) => console.log(error));
-      };
-      fetchingWeather();
+    let getKey = data.Key;
 
 
-      
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setGetCity(input)
+        const fetchingWeather = () => {
+            fetch(
+                `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${getKey}?apikey=${API_KEY}&metric=true`,{ mode: 'no-cors' }
+            )
+                .then((res) => {
+                    if (res.ok) {
+                        console.log(res.status);
+                        return res.json();
+                    } else {
+                        if (res.status === 404) {
+                            return alert("error");
+                        }
+                        alert("something went wrong");
+                        throw new Error("you have an error");
+                    }
+                })
+                .then((result) => {
+                    console.log('weather', result);
+                    let { Value } = result.DailyForecasts[0].Temperature.Maximum;
+                    let { Text } = result.Headline;
+                    setWeather({ Value, Text });
+                })
+                .catch((error) => console.log(error));
+        };
+        fetchingWeather();
 
 
- /*  useEffect(() => {
-    
-  }, []); */
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-      </form>
-      <div>{data.LocalizedName}</div>
-    </div>
-  );
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            </form>
+            <p>{data.ID}</p>
+            <div>{data.LocalizedName}</div>
+            <p>{weather.Value}</p>
+            <p>{weather.Text}</p>
+        </div>
+    );
 }
 
 export default Home;
